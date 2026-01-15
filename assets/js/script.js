0b8dc59c5677f502a63cd07bd99f6bb833f6a519
rezafauzan593@gmail.com <rezafauzan593@gmail.com>
2026-01-15 11:37:39 +0700

    Fix: Fix issue when replacing glass content that has content, now we can just put taken content to empty glass

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,3 @@
-const gelasA = document.getElementById('gelas-a')
-const gelasB = document.getElementById('gelas-b')
-const gelasTransit = document.getElementById('gelas-transit')
 const gelasAContent = document.querySelector('#gelas-a div')
 const gelasBContent = document.querySelector('#gelas-b div')
 const gelasTransitContent = document.querySelector('#gelas-transit div')
@@ -9,34 +6,46 @@ let temp = ''
 
 gelasAContent.addEventListener('click',
     () => {
-        if (temp === '') {
-            temp = gelasAContent.getAttribute('class')
-            gelasAContent.setAttribute('class', '')
-        } else if (temp !== '' && gelasAContent.getAttribute('class') === '') {
-            gelasAContent.setAttribute('class', temp)
-            temp = ''
+        if (temp !== '' && gelasAContent.getAttribute('class').length > 0) {
+            alert("gelas ada isinya")
+        } else {
+            if (temp === '') {
+                temp = gelasAContent.getAttribute('class')
+                gelasAContent.setAttribute('class', '')
+            } else if (temp !== '') {
+                gelasAContent.setAttribute('class', temp)
+                temp = ''
+            }
         }
     }
 )
 gelasBContent.addEventListener('click',
     () => {
-        if (temp === '') {
-            temp = gelasBContent.getAttribute('class')
-            gelasBContent.setAttribute('class', '')
-        } else if (temp !== '') {
-            gelasBContent.setAttribute('class', temp)
-            temp = ''
+        if (temp !== '' && gelasBContent.getAttribute('class').length > 0) {
+            alert("gelas ada isinya")
+        } else {
+            if (temp === '') {
+                temp = gelasBContent.getAttribute('class')
+                gelasBContent.setAttribute('class', '')
+            } else if (temp !== '') {
+                gelasBContent.setAttribute('class', temp)
+                temp = ''
+            }
         }
     }
 )
 gelasTransitContent.addEventListener('click',
     () => {
-        if (temp === '') {
-            temp = gelasTransitContent.getAttribute('class')
-            gelasTransitContent.setAttribute('class', '')
-        } else if (temp !== '') {
-            gelasTransitContent.setAttribute('class', temp)
-            temp = ''
+        if (temp !== '' && gelasTransitContent.getAttribute('class').length > 0) {
+            alert("gelas ada isinya")
+        } else {
+            if (temp === '') {
+                temp = gelasTransitContent.getAttribute('class')
+                gelasTransitContent.setAttribute('class', '')
+            } else if (temp !== '') {
+                gelasTransitContent.setAttribute('class', temp)
+                temp = ''
+            }
         }
     }
 )
